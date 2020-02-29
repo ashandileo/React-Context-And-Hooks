54c90881d7734b2f8daafc883b91a5a2912d3bc7
AshandiLeonadi <ashandileonadi2001@gmail.com>
2020-02-29 15:23:19 +0700

    Menggunakan Consumer untuk consume Context

--- my-react/src/components/Navbar.js
+++ my-react/src/components/Navbar.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ThemeContext } from '../contexts/ThemeContext'
 
 
-class Navbar extends Component {
-
-    static contextType = ThemeContext
-
-    render() { 
-        const { isLightTheme, light, dark } = this.context
-        const theme = isLightTheme ? light : dark
+const Navbar = () => {
         return (
-            <nav style={{ background: theme.ui, color: theme.syntax }}>
-                <h1>Content App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
+            <ThemeContext.Consumer>{(context) => {
+                const { isLightTheme, light, dark } = context
+                const theme = isLightTheme ? light : dark
+                return (
+                    <nav style={{ background: theme.ui, color: theme.syntax }}>
+                        <h1>Content App</h1>
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </nav>
+                )
+            }}</ThemeContext.Consumer>
         );
-    }
 }
  
 export default Navbar;
