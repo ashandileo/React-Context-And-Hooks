b5e6a24c69f13d0cd70cfa5f82266f6df5ec457e
AshandiLeonadi <ashandileonadi2001@gmail.com>
2020-02-29 15:00:46 +0700

    Mengakses ThemeContext pada class component

--- my-react/src/components/BookList.js
+++ my-react/src/components/BookList.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext'
 
 class BookList extends Component {
+
+    static contextType = ThemeContext
+
     render() { 
+        const { isLightTheme, light, dark } = this.context
+        const theme = isLightTheme ? light : dark
         return (
-            <div className="book-list">
+            <div className="book-list" style={{ backgroundColor: theme.bg, color: theme.syntax }}>
                 <ul>
-                    <li>book 1</li>
-                    <li>book 2</li>
-                    <li>book 3</li>
+                    <li style={{ backgroundColor: theme.ui }}>book 1</li>
+                    <li style={{ backgroundColor: theme.ui }}>book 2</li>
+                    <li style={{ backgroundColor: theme.ui }}>book 3</li>
                 </ul>
             </div>
         );
